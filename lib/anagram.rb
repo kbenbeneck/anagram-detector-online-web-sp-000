acb9c2482b6a61e8df0d2837b703fffc0425da17
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-22 23:18:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,12 @@
 class Anagram
+
  attr_accessor :name
 
  def initialize(word)
    @name = word
  end
 
- def mathch(array)
+ def match(array)
    array.select { |a| a.split("").sort == @name.split("").sort}
  end
 end
